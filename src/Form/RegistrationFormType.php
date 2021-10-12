2ab491942ca45a2aff5d6819a30ab41384c04a57
2alheure <jojo77.juv@live.fr>
2021-10-12 10:58:02 +0200

    Correction bug Register + bouton crééer article invisible

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -10,14 +10,16 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class RegistrationFormType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
+            ->add('email', EmailType::class)
             ->add('pseudo', TextType::class)
             ->add('password', RepeatedType::class, [
                 'mapped' => false,
